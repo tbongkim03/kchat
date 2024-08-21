a62cd4cb2bfea41c5ccc13150410d2d4dbd4f562
tbongkim03 <tbongkim03@gmial.com>
2024-08-21 17:11:27 +0900

    offset save

--- src/kchat/kafka/pro.py
+++ src/kchat/kafka/pro.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 import time
 import json
 
+
 pro = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
@@ -13,3 +14,22 @@ for i in range(10):
     pro.flush()
 end = time.time()
 print('[DONE]: ', end - start)
+
+"""
+def kchat_p():
+    pro = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        value_serializer=lambda x: json.dumps(x).encode('utf-8')
+        )
+
+    #start = time.time()
+    while True:
+        inp=input("입력: ")
+        data={"message": inp}
+        pro.send('message', value=data)
+        pro.flush()
+        if inp == 'exit':
+            print('종료합니다.')
+            break
+    #end = time.time()
+"""
